bb675485c00b4f6a38f9ce9eda51f944847de725
gon17 <gon17@DESKTOP-KHGVB5M>
2018-11-25 20:37:24 +0100

    terminada

--- src/paquete/P4_GonzaloIzuzquiza.java
+++ src/paquete/P4_GonzaloIzuzquiza.java
@@ -1,7 +1,8 @@
 package paquete;
 
 import java.awt.Color;
-
+import java.io.File;
+import javax.swing.JFileChooser;
 
 /**
  * @author Gonzalo Izuzquiza
@@ -9,32 +10,79 @@ import java.awt.Color;
 public class P4_GonzaloIzuzquiza extends javax.swing.JFrame {
     
     XPATH gesXpath = new XPATH();    
+    File fichero = null;
+    String escribe;
+    
     public P4_GonzaloIzuzquiza() {
         initComponents();
         this.getContentPane().setBackground(Color.RED); // damos color al fondo del jFrame
         this.setLocationRelativeTo(null); // con esto hacemos que la pnatalla se abra en el centro de la pantalla
-        setSize(840, 540); //con esto damo un tamñano pasando dos parametros, siendo ancho y alto, a la pantalla
+        setSize(650, 540); //con esto damo un tamñano pasando dos parametros, siendo ancho y alto, a la pantalla
     }
     
-    
+    private File abrirDocumento() {
+        
+        int rv;
+        JFileChooser fc = new JFileChooser();
+        fc.setMultiSelectionEnabled(false);
+        fc.setDialogType(JFileChooser.OPEN_DIALOG);
+        rv = fc.showOpenDialog(this);
+        if (rv == JFileChooser.APPROVE_OPTION) {
+            fichero = fc.getSelectedFile();
+            cargado.setText("Archivo cargado correctamente");
+        }
+        return fichero;
+    }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        textoAmostrar = new javax.swing.JTextArea();
         xpath_texto = new javax.swing.JLabel();
+        abrir = new javax.swing.JButton();
+        cargado = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        escribir_consulta = new javax.swing.JTextArea();
+        ejecutar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        resutado = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        textoAmostrar.setColumns(20);
-        textoAmostrar.setRows(5);
-        jScrollPane1.setViewportView(textoAmostrar);
 
         xpath_texto.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         xpath_texto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         xpath_texto.setText("XPATH");
+
+        abrir.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        abrir.setText("Abrir archivo XML");
+        abrir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                abrirActionPerformed(evt);
+            }
+        });
+
+        cargado.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        cargado.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+        escribir_consulta.setColumns(20);
+        escribir_consulta.setRows(5);
+        jScrollPane1.setViewportView(escribir_consulta);
+
+        ejecutar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        ejecutar.setText("Ejecutar");
+        ejecutar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ejecutarActionPerformed(evt);
+            }
+        });
+
+        resutado.setColumns(20);
+        resutado.setRows(5);
+        jScrollPane2.setViewportView(resutado);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel1.setText("Escribe tu consulta:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -43,24 +91,55 @@ public class P4_GonzaloIzuzquiza extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 415, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(ejecutar, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(134, 134, 134)
-                        .addComponent(xpath_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 512, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(xpath_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(abrir, javax.swing.GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE)
+                            .addComponent(cargado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(xpath_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(xpath_texto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(abrir, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(cargado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 33, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addComponent(ejecutar, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void abrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_abrirActionPerformed
+        abrirDocumento();        
+    }//GEN-LAST:event_abrirActionPerformed
+
+    private void ejecutarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ejecutarActionPerformed
+        gesXpath.EjecutaXPath(fichero, escribir_consulta.getText());
+        resutado.setText(gesXpath.salida);
+    }//GEN-LAST:event_ejecutarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -76,8 +155,14 @@ public class P4_GonzaloIzuzquiza extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton abrir;
+    private javax.swing.JLabel cargado;
+    private javax.swing.JButton ejecutar;
+    private javax.swing.JTextArea escribir_consulta;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea textoAmostrar;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea resutado;
     private javax.swing.JLabel xpath_texto;
     // End of variables declaration//GEN-END:variables
 
--- src/paquete/XPATH.java
+++ src/paquete/XPATH.java
@@ -11,33 +11,39 @@ import org.w3c.dom.*;
 public class XPATH {
 
     Document doc;
-    File fichero;
+    String salida = "";
 
-    public int EjecutaXPath() {
-        String salida = "";
+    public int EjecutaXPath(File fichero, String consulta) {
+
         try {
             //Crea un objeto DocumentBuilderFactory para el DOM (JAXP)
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             //Crear un árbol DOM (parsear) con el archivo LibrosXML.xml
-            Document XMLDoc = factory.newDocumentBuilder().parse(new File("LibrosXML.xml"));
+            Document XMLDoc = factory.newDocumentBuilder().parse(fichero);
             //Crea el objeto XPath
             XPath xpath = XPathFactory.newInstance().newXPath();
             //Crea un XPathExpression con la consulta deseada
-            XPathExpression exp = xpath.compile("/Libros/*/Autor");
+            XPathExpression exp = xpath.compile(consulta);
             //Ejecuta la consulta indicando que se ejecute sobre el DOM y que devolverá
             //el resultado como una lista de nodos.
             Object result = exp.evaluate(XMLDoc, XPathConstants.NODESET);
             NodeList nodeList = (NodeList) result;
             //Ahora recorre la lista para sacar los resultados
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                salida = salida + "\n"
-                        + nodeList.item(i).getChildNodes().item(0).getNodeValue();
+            if (consulta.equals("/Libros/Libro")) {
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    salida = salida + "\n" + nodeList.item(i).getTextContent();
+                    salida = salida + "\n" + nodeList.item(i).getAttributes().item(0).getNodeValue();
+
+                }
+            } else {
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    salida = salida + "\n" + nodeList.item(i).getTextContent();
+
+                }
             }
-            System.out.println(salida);
             return 0;
         } catch (Exception e) {
-            System.out.println("Error: " + e.toString());
-            return 0;
+            return -1;
         }
     }
 
